5fc4fbb3e08174c8a1bfb21d7d49347a94445e97
yaozheng600 <yaozheng_Levi@163.com>
2023-10-05 18:15:18 +0200

    add requirement

--- pages/2_Exploration.py
+++ pages/2_Exploration.py
@@ -11,7 +11,7 @@ import Interface_config as ic
 from Anony.preserver import Preserver
 from Measurement import Measurement
 
-with open(st.secrets["GOOGLE_CLOUD_CREDENTIALS_JSON_FILE"], "r") as file:
+with open(st.secrets["GOOGLE_CLOUD_CREDENTIALS_JSON"], "r") as file:
     credential_path = json.load(file)
 client = storage.Client.from_service_account_json(credential_path)
 bucket_name = "survey_masterarbeit"

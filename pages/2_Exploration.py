6a4cc9a9c46a36510a5ec4a86201b6acbf47a542
yaozheng600 <yaozheng_Levi@163.com>
2023-10-05 18:29:51 +0200

    add requirement

--- pages/2_Exploration.py
+++ pages/2_Exploration.py
@@ -1,5 +1,4 @@
 import json
-import os
 import tempfile
 from google.cloud import storage
 import streamlit as st
@@ -12,8 +11,8 @@ import Interface_config as ic
 from Anony.preserver import Preserver
 from Measurement import Measurement
 
-with open(os.environ["GOOGLE_CLOUD_CREDENTIALS_JSON"], "r") as file:
-    credential_path = json.load(file)
+
+credential_path = st.secrets["GOOGLE_CLOUD_CREDENTIALS_JSON"]
 client = storage.Client.from_service_account_json(credential_path)
 bucket_name = "survey_masterarbeit"
 csv_file_name = "survey_1_iteration"
